df99d7cb073e8ef7749b32e66d40fd513e03ebc2
lvmingyin <lvmingyin@vip.qq.com>
2017-08-09 15:46:34 +0800

    baseFindIndex

--- src/.internal/baseFindIndex.js
+++ src/.internal/baseFindIndex.js
@@ -7,13 +7,15 @@
  * @param array 查找的数组
  * @param predicate 查找的方法
  * @param fromIndex 开始位置
- * @param fromRight 结束位置
+ * @param [fromRight] 查找方向 false 从左往右  true 从右往左
  */
-export default function(array, predicate, fromIndex, fromRight){
-  const {length} = array;
-  let index = fromIndex + (fromRight ? 1: -1);
-  while((fromRight ? index -- : ++index < length)){
-    if(predicate(array[index], index, array)){
+export default function (array, predicate, fromIndex, fromRight) {
+  const { length } = array; // 获得数组长度
+  // 开始位置
+  let index = fromIndex + (fromRight ? 1 : -1);
+  // 如果从右边开始查找，递减并且递减前大于0，从左边开始，递增并且递增后小于数组长度
+  while ((fromRight ? index-- : ++index < length)) {
+    if (predicate(array[index], index, array)) {
       return index;
     }
   }
